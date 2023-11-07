a862df65ce2585d120f8495aec54e71e56768114
Manigandan C <manigandan.c2003@gmail.com>
2023-11-07 14:14:41 +0530

    Added Azure Link

--- server/server.js
+++ server/server.js
@@ -32,12 +32,7 @@ app.use(
   })
 );
 
-app.use(
-  cors({
-    origin: "https://nexus-freelance.azurewebsites.net",
-    credentials: true,
-  })
-);
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
